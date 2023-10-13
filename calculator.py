79b29e589e7110680b5043652b40aa25d62a9596
Harsh Khullar <103531236+harshkhulla@users.noreply.github.com>
2023-10-14 01:25:14 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import math
+
+def log(x,y):
+    return math.log(x,y)
+
+def sine(x,y):
+    return math.sin(x,y)
+
+def cosine(x,y):
+    return math.cos(x,y)
+
+def squarted(x,y):
+   return x **y 
+
+
